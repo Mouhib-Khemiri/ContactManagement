a7be31825f90a0b48831751e3f1a51492d15a0b4
Mouhib Khemiri <khemiri.mouhib43@gmail.com>
2021-05-07 22:10:55 +0100

    Added solution

---
+++ ContactManagement/ContactManagementLibrary/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagementLibrary
+{
+    public class Contact
+    {
+        /// <summary>
+        /// Contact ID.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// FirstName.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// LastName.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Address.
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Tel.
+        /// </summary>
+        public string Tel { get; set; }
+
+    }
+}
